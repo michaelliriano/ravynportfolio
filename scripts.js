4251ecb1100a8e4f335a8e69081b2a1078c85872
Michael Liriano <51761089+michaelliriano@users.noreply.github.com>
2020-01-16 00:42:55 -0500

    added reveal on scroll

--- scripts.js
+++ scripts.js
@@ -11,3 +11,13 @@ selectElement('.open').addEventListener('click', () => {
 selectElement('.close').addEventListener('click', () => {
   selectElement('.nav-list').classList.remove('active');
 });
+
+// Reveal on scroll
+ScrollReveal().reveal('.hero');
+ScrollReveal().reveal('.star-gazing', { delay: 500 });
+ScrollReveal().reveal('.hiking', { delay: 500 });
+ScrollReveal().reveal('.camping', { delay: 500 });
+ScrollReveal().reveal('.testimonial-customer', { delay: 500 });
+ScrollReveal().reveal('.testimonial-text-box', { delay: 500 });
+ScrollReveal().reveal('.begin-adventure', { delay: 300 });
+ScrollReveal().reveal('.adventure-grid-item', { delay: 300 });
